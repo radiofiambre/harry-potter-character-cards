7f991665b962f503265034b3ed261e4b261fe8d0
Patricia <patriciaparreirasainz@gmail.com>
2025-03-14 14:13:27 +0100

    updated Sass

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/modulo-3-evaluacion-final-radiofiambre/",
+  base: "/harry-potter-character-cards/",
   server: {
     watch: {
       usePolling: true,
